171cc266c0db039a1874f334623cb00a5795ff30
javifieldeas2017 <javifieldeas2017@outlook.es>
2017-07-07 00:47:41 +0200

    Commit Tarea01

--- EjercicioTarea01/Class1.cs
+++ EjercicioTarea01/Class1.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace EjercicioTarea01
 {
-    public class Class1
+    public class ClaseDatos
     {
+        public string texto { get; set; }
+        public int a { get; set; } 
+        public int b { get; set; }
+
+    }
+
+    public class ClaseLogica
+    {
+        public string ConcatenarAtributos(ClaseDatos obj)
+        {
+            string cadena = obj.texto+ "-" + obj.a + "-" + obj.b;
+            return cadena;
+        }
     }
 }
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EjercicioTarea01;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ClaseDatos sut = new ClaseDatos();
+            ClaseLogica herramienta = new ClaseLogica();
+            sut.texto = "x";
+            sut.a = 5;
+            sut.b = 6;
+
+            String resultado = herramienta.ConcatenarAtributos(sut);
+            String comparacion = "x-5-6";
+
+            Assert.AreEqual(resultado, comparacion);
+        }
+    }
+}
